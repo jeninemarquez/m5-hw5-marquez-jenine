9724ddd92f6968e6cec2fbddff8997cca4735227
Jenine Marquez <jeninemarquez@Jenines-MacBook-Air.local>
2020-02-10 01:01:48 -0500

    Invoke function addToppings

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 var pizza = {
-    crust = 'neapolitan',
-    cheese = 'mozzarella',
-    sauce = 'tomato',
-    addToppings = function() {console.log('Please add pepperoni and sausage')},
+    crust:'neapolitan',
+    cheese: 'mozzarella',
+    sauce: 'tomato',
+    addToppings: function() {
+        console.log('Please add pepperoni and sausage');
+    }
 };
 
 function orderPizza() {
     console.log(pizza.crust);
     console.log(pizza.cheese);
     console.log(pizza.sauce);
-    console.log(pizza.addToppings);
+    console.log(pizza.addToppings());
 }
 
 console.log(orderPizza());
